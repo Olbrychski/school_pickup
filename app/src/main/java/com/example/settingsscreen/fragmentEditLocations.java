7bce1a102f85ae076ce5fd7866f3d68412e0c771
frankmakz <frankosoro60@gmail.com>
2021-09-24 14:51:59 +0200

    AdditionL Screens

--- app/src/main/java/com/example/settingsscreen/fragmentEditLocations.java
+++ app/src/main/java/com/example/settingsscreen/fragmentEditLocations.java
@@ -8,12 +8,24 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class fragmentEditLocations extends Fragment {
+
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_edit_location, container, false);
+
     }
+
+
 }
