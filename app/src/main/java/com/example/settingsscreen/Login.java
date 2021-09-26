0d79e8e7564af13f607879d4094dafccf006cf07
frankmakz <frankosoro60@gmail.com>
2021-09-27 02:17:52 +0800

    login splash

--- app/src/main/java/com/example/settingsscreen/Login.java
+++ app/src/main/java/com/example/settingsscreen/Login.java
@@ -2,10 +2,17 @@ package com.example.settingsscreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+    private Button btn_login;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +20,15 @@ public class Login extends AppCompatActivity {
         //Remove status bar
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+
+        btn_login = findViewById(R.id.button_login);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
