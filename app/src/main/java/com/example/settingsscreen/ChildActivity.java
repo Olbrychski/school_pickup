095f031168b110cc5626c39520e83b1864b67a15
denniskimtai <denniskimtai1@gmail.com>
2021-10-05 23:03:29 +0300

    Child status

---
+++ app/src/main/java/com/example/settingsscreen/ChildActivity.java
@@ -0,0 +1,91 @@
+package com.example.settingsscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+
+import android.graphics.drawable.GradientDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class ChildActivity extends AppCompatActivity implements TabLayout.OnTabSelectedListener {
+
+    //This is our tablayout
+    private TabLayout tabLayout;
+
+    //This is our viewPager
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        setContentView(R.layout.activity_child);
+
+//        //Adding toolbar to the activity
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+        //Initializing the tablayout
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+
+        TabLayout.Tab tabHome = tabLayout.newTab().setText("Child Status");
+        TabLayout.Tab tabEmprestimos = tabLayout.newTab().setText("History");
+        TabLayout.Tab tabPersonal = tabLayout.newTab().setText("Bus Info");
+
+        View root = tabLayout.getChildAt(0);
+        if (root instanceof LinearLayout) {
+            ((LinearLayout) root).setShowDividers(LinearLayout.SHOW_DIVIDER_MIDDLE);
+            GradientDrawable drawable = new GradientDrawable();
+            drawable.setColor(getResources().getColor(R.color.grey));
+            drawable.setSize(2, 1);
+            ((LinearLayout) root).setDividerPadding(10);
+            ((LinearLayout) root).setDividerDrawable(drawable);
+        }
+
+        tabLayout.addTab(tabHome);
+        tabLayout.addTab(tabEmprestimos);
+        tabLayout.addTab(tabPersonal);
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+
+        //Initializing viewPager
+        viewPager = (ViewPager) findViewById(R.id.pager);
+
+        //Creating our pager adapter
+        TabMenuPagerAdapter adapter = new TabMenuPagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+
+        //Adding adapter to pager
+        viewPager.setAdapter(adapter);
+
+        //Adding onTabSelectedListener to swipe views
+        tabLayout.setOnTabSelectedListener(this);
+
+    }
+
+    @Override
+    public void onTabSelected(TabLayout.Tab tab) {
+
+        viewPager.setCurrentItem(tab.getPosition());
+
+    }
+
+    @Override
+    public void onTabUnselected(TabLayout.Tab tab) {
+
+    }
+
+    @Override
+    public void onTabReselected(TabLayout.Tab tab) {
+
+    }
+}
--- app/src/main/java/com/example/settingsscreen/adapterMyChildren.java
+++ app/src/main/java/com/example/settingsscreen/adapterMyChildren.java
@@ -1,10 +1,12 @@
 package com.example.settingsscreen;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -22,6 +24,7 @@ public class adapterMyChildren  extends RecyclerView.Adapter<adapterMyChildren.E
         public ImageView childImageView;
         public TextView mTextViewChildName;
         public TextView mTextViewChildClass, txtViewChildStatus;
+        public LinearLayout childLayout;
 
 
         public ExampleViewHolder(@NonNull View itemView) {
@@ -30,6 +33,7 @@ public class adapterMyChildren  extends RecyclerView.Adapter<adapterMyChildren.E
             mTextViewChildName = itemView.findViewById(R.id.textview);
             mTextViewChildClass = itemView.findViewById(R.id.textViewClass);
             txtViewChildStatus = itemView.findViewById(R.id.textViewProgress);
+            childLayout = itemView.findViewById(R.id.child_layout);
 
         }
     }
@@ -67,7 +71,7 @@ public class adapterMyChildren  extends RecyclerView.Adapter<adapterMyChildren.E
 
             case "In Bus":
 
-                holder.txtViewChildStatus.setTextColor(ContextCompat.getColor(mCtx, R.color.purple_200));
+                holder.txtViewChildStatus.setTextColor(ContextCompat.getColor(mCtx, R.color.primary));
 
                 break;
 
@@ -78,6 +82,16 @@ public class adapterMyChildren  extends RecyclerView.Adapter<adapterMyChildren.E
                 break;
 
         }
+
+        holder.childLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(mCtx, ChildActivity.class);
+                mCtx.startActivity(intent);
+
+            }
+        });
 
     }
 
